3c59af9ccadb4a2f1904a53ba3db0b46b758c826
DaanBruijn <332951@student.mboutrecht.nl>
2023-01-09 10:51:35 +0100

    Inital Commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database constanten
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'be-mvcdb');
+
+// Het pad naar de app map
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// De URL van de site
+define('URLROOT', 'http://be-mvcframework.local');
+
+// Naam van de site
+define('SITENAME', 'BE-MVC-Framework');
